fe8c2ab0e129eb935d36bd9179d271debcf7fbfb
seokjin lim <hopesukjin@gmail.com>
2021-03-24 23:40:38 +0900

    test

--- kakao_msg/msg_test.py
+++ kakao_msg/msg_test.py
@@ -1,28 +1,28 @@
-import json
 import requests
-
-url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
-
-# 사용자 토큰
-headers = {
-    "Authorization": "Bearer " + <access token>
-}
-
-
-data = {
-    "template_object" : json.dumps({ "object_type" : "text",
-                                     "text" : "Hello, world!",
-                                     "link" : {
-                                                 "web_url" : "www.naver.com"
-                                              }
-    })
-}
-
-response = requests.post(url, headers=headers, data=data)
-print(response.status_code)
-if response.json().get('result_code') == 0:
-    print('메시지를 성공적으로 보냈습니다.')
-else:
-    print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ' + str(response.json()))
-  
-  
+import json
+from pykrx import stock
+from datetime import date, datetime, timedelta
+import pandas as pd
+def post_message(channel, text): 
+    SLACK_BOT_TOKEN = ""
+    headers = {
+        'Content-Type': 'application/json', 
+        'Authorization': 'Bearer ' + SLACK_BOT_TOKEN
+        }
+    payload = {
+        'channel': channel,
+        'text': text
+        }
+    r = requests.post('https://slack.com/api/chat.postMessage', 
+        headers=headers, 
+        data=json.dumps(payload)
+        )
+to_day = date.today().isoformat().replace('-', '')
+df = stock.get_market_ohlcv_by_date("20210320",to_day, "252670", "d")
+df = pd.DataFrame(df)
+text  = df.iloc[-1][3]
+text2 = int(text)*9466 - 21239545
+a = int(df.iloc[-1][3])
+b = int(df.iloc[-2][3])
+c = round((a - b) / a * 100,2)
+post_message("financefroject", '현제가 :'+format(text, ',')+'원 ('+format(c, ',')+'%)'+ '\n보유수량 :9,466주'+'\n손익 :'+format(text2, ',')+'원')
